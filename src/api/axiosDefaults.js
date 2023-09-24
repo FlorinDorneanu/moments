e2e8791baa8640eec238574911a844ca2f033419
FlorinDorneanu <florindorneanuu1@gmail.com>
2023-09-24 14:52:34 +0000

    Commit

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://moments-moments-bdf530118a56.herokuapp.com'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "https://drf-api-api-81cac82072ee.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
